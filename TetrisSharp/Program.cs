3c60d141058a9e7eef46d308824484fe2d74ce20
Ana Grigorcea <ana.grigorcea@gmail.com>
2019-12-08 22:08:40 +0200

    Created project

---
+++ TetrisSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TetrisSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
